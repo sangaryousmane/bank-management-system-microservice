ff9355bdc08b9c795aa1d0d07fe76c6e94eb871d
ousmane <sangaryousmane@gmail.com>
2023-12-08 18:55:53 +0800

    Add dependency for rate limiting in api gateway

--- api-gateway/src/main/java/org/atalibdev/getway/GatewayApplication.java
+++ api-gateway/src/main/java/org/atalibdev/getway/GatewayApplication.java
@@ -1,7 +1,11 @@
 package org.atalibdev.getway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
@@ -15,21 +19,31 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+
     @Bean
-    RouteLocator router(RouteLocatorBuilder builder) {
+    public Customizer<Resilience4JCircuitBreakerFactory> factory() {
+        return fact -> fact.configureDefault(
+                id -> new Resilience4JConfigBuilder(id)
+                        .circuitBreakerConfig(
+                                CircuitBreakerConfig.ofDefaults())
+                        .build());
+    }
+
+    @Bean
+    public RouteLocator router(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/api/v1/accounts/**")
-                        .filters(f -> f.circuitBreaker(c-> {
+                        .filters(f -> f.circuitBreaker(c -> {
                             c.setName("ACCOUNT-SERVICE");
                             c.setFallbackUri("forward:/accountServiceFallBack");
                         })).uri("lb://ACCOUNT-SERVICE"))
                 .route(r -> r.path("/api/v1/customers/**")
                         .filters(f -> f.circuitBreaker(c -> {
-                           c.setName("CUSTOMER-SERVICE");
-                           c.setFallbackUri("forward:/accountServiceFallBack");
+                            c.setName("CUSTOMER-SERVICE");
+                            c.setFallbackUri("forward:/customerServiceFallBack");
                         })).uri("lb://CUSTOMER-SERVICE"))
                 .route(r -> r.path("/api/v1/consumerApp/**")
-                        .filters(f -> f.circuitBreaker(c ->{
+                        .filters(f -> f.circuitBreaker(c -> {
                             c.setName("WEB-APPLICATION-CONSUMER");
                             c.setFallbackUri("forward:/webServiceFallBack");
                         })).uri("lb://WEB-APPLICATION-CONSUMER"))
--- customer-service/src/main/java/com/ousmane/customerservice/external/consumer/AccountService.java
+++ customer-service/src/main/java/com/ousmane/customerservice/external/consumer/AccountService.java
@@ -1,15 +1,28 @@
 package com.ousmane.customerservice.external.consumer;
 
+import com.ousmane.customerservice.exceptions.CustomerNotFoundException;
 import com.ousmane.customerservice.external.Account;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.security.auth.login.AccountNotFoundException;
 import java.util.List;
 
+
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "account-service")
 public interface AccountService {
 
     @GetMapping("/api/v1/accounts/customer/{customerId}")
     List<Account> getAccountList(@PathVariable Integer customerId);
+
+
+    default void fallback(CustomerNotFoundException a){
+        throw new CustomerNotFoundException(
+                "customer service not accepting request",
+                HttpStatus.NOT_FOUND);
+    }
 }
