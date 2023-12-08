84e95f930c3bc64f3c97af0a409fc8bcc2f9de8b
ousmane <sangaryousmane@gmail.com>
2023-12-08 20:08:34 +0800

    add key resolver bean

--- api-gateway/src/main/java/org/atalibdev/getway/GatewayApplication.java
+++ api-gateway/src/main/java/org/atalibdev/getway/GatewayApplication.java
@@ -1,5 +1,4 @@
 package org.atalibdev.getway;
-
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,34 +8,44 @@ import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+
 
 @SpringBootApplication
 public class GatewayApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
 
     @Bean
+    public KeyResolver keyResolver(){
+        return resolver -> Mono.just("userKey");
+    }
+
+    @Bean
     public Customizer<Resilience4JCircuitBreakerFactory> factory() {
         return fact -> fact.configureDefault(
                 id -> new Resilience4JConfigBuilder(id)
-                        .circuitBreakerConfig(
-                                CircuitBreakerConfig.ofDefaults())
+                        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
                         .build());
     }
 
     @Bean
     public RouteLocator router(RouteLocatorBuilder builder) {
+
         return builder.routes()
                 .route(r -> r.path("/api/v1/accounts/**")
                         .filters(f -> f.circuitBreaker(c -> {
                             c.setName("ACCOUNT-SERVICE");
                             c.setFallbackUri("forward:/accountServiceFallBack");
-                        })).uri("lb://ACCOUNT-SERVICE"))
+                        }))
+                        .uri("lb://ACCOUNT-SERVICE"))
                 .route(r -> r.path("/api/v1/customers/**")
                         .filters(f -> f.circuitBreaker(c -> {
                             c.setName("CUSTOMER-SERVICE");
