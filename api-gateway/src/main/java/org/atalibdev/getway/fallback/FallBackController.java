4291f37db225a3fb7b65e6af90dd4f904e4f0d9a
ousmane <sangaryousmane@gmail.com>
2023-12-08 17:55:59 +0800

    Add fallback methods to api gateways

---
+++ api-gateway/src/main/java/org/atalibdev/getway/fallback/FallBackController.java
@@ -0,0 +1,18 @@
+package org.atalibdev.getway.fallback;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackController {
+
+    @GetMapping("/customerServiceFallBack")
+    public String customerServiceFallBack(){
+        return "Customer service is down!!";
+    }
+
+    @GetMapping("/accountServiceFallBack")
+    public String accountServiceFallBack(){
+        return "Account service is down!!!";
+    }
+}
